79811f0cb02e4efe44aaad0bb9ad3145fd6f9ae4
AntonBangoura <103506746+AntonBangoura@users.noreply.github.com>
2022-07-21 14:24:46 +0200

    Update weatherServices.js

--- src/services/weatherServices.js
+++ src/services/weatherServices.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "11410c1f30aeca9b43d13c5108e74f67";
+const API_KEY = "MY_API_KEY";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
